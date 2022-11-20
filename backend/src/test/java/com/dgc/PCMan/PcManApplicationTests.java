5c9b1f25f70311debf3a729cb49116451af6516f
Denison G. Corbal <denison.corbal@gmail.com>
2022-11-20 10:07:50 -0300

    Base project Back(Java) + Front(React)

---
+++ backend/src/test/java/com/dgc/PCMan/PcManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgc.PCMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
