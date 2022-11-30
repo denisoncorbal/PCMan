db7bd2c048131a5c7dc1ad7793a1045b88389f0b
Denison G. Corbal <denison.corbal@gmail.com>
2022-11-30 09:23:28 -0300

    basic docker development configuration

--- backend/src/main/java/com/dgc/PCMan/PcManApplication.java
+++ backend/src/main/java/com/dgc/PCMan/PcManApplication.java
@@ -2,9 +2,15 @@ package com.dgc.PCMan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class PcManApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PcManApplication.class, args);
