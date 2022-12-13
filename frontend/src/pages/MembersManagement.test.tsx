917c67948d703ad476dc0db6cc3c7fc6df1ffd40
Denison G. Corbal <denison.corbal@gmail.com>
2022-12-13 12:00:20 -0300

    Add more tests and jest code coverage

---
+++ frontend/src/pages/MembersManagement.test.tsx
@@ -0,0 +1,14 @@
+import { render, renderHook } from "@testing-library/react";
+import MembersManagement from "./MembersManagement";
+
+describe("MembersManagement", ()=>{
+    it("should render with preset data", () => {
+        const memberTest = {id: 1, name: "teste", telephone: "(00) 0 0000-0000", email: "teste@teste.com"};
+        //TODO
+        expect(true).toBe(true);
+    })
+    it("should render on integration with backend", () => {
+        //TODO
+        expect(true).toBe(true);
+    })
+})
--- frontend/src/pages/MembersManagement.tsx
+++ frontend/src/pages/MembersManagement.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FaUserEdit} from 'react-icons/fa'
 import { MemberResumeContainer, MembersManagementContainer } from './membersManagementStyles';
 
@@ -6,33 +6,28 @@ interface MemberManagementProps{
     name: string
 }
 
+interface Member{
+    id?: number,
+    name: string,
+    telephone: string,
+    email: string
+}
+
 const MemberManagement = (props: MemberManagementProps) => {
+    const [members, setMembers] = useState<Member[] | null>(null);
     return (
         <MembersManagementContainer>
         {
-            //cabem 20 elementos na tela
-            // TODO elemento reutilizável de exibição de usuário
+            members ? members.map((member)=>{return(
+                <MemberResumeContainer key={member.id}>
+                    <FaUserEdit />
+                    <p>{member.name}</p>
+                    <p>{member.telephone}</p>
+                    <p>{member.email}</p>
+                </MemberResumeContainer>
+            )} )
+            : <></>
         }
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>
-            <MemberResumeContainer><FaUserEdit /> <p>Fulano de Tal</p> <p>(xx) x xxxx-xxxx</p> <p>fulano@fulano.com</p> </MemberResumeContainer>            
         </MembersManagementContainer>
     );
 };
